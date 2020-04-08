45805dce78af31e9cda096e8c4f596d18ae59adf
wuhao <wuhaotx@feinno.com>
2020-04-08 15:16:13 +0800

    修改地址

--- docker/0.1.0/docker-run.sh
+++ docker/0.1.0/docker-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker stop dubbo-admin
 docker rm `docker ps -a -q`
-docker run -d  --privileged=true -p 8091:8080 --name dubbo-admin 10.10.208.193:5000/dubbo-admin:20191204113501
+docker run -d  --privileged=true -p 8091:8080  --env ZK_URL='10.10.220.120:7998' --name dubbo-admin 10.10.208.193:5000/dubbo-admin:20200408151549
 
 docker logs -f dubbo-admin
